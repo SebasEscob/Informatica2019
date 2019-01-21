86bc1f49af94ee8ba3757464207a3f77ec4ce64b
BrandonRH17 <rodriguez181048@unis.edu.gt>
2019-01-21 07:53:35 -0600

    Laboratorio1

---
+++ HelloWorld/HelloWorld.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Mensaje.hh"
+#include "Mensaje1.hh"
+#include "Valor3.hh"
+
+int main(){
+
+    std::printf(MENSAJE); 
+    std::printf("%s, %i\n", MENSAJE1, VALOR3);    
+
+}
---
+++ HelloWorld/Valor3.hh
@@ -0,0 +1,4 @@
+#include "Valor1.hh"
+#include "Valor2.hh"
+
+#define VALOR3 VALOR1 + VALOR2
